872cb6d50144ae1e1cc2b275557dd8844ee08119
WennnTing <wendy1000203@gmail.com>
2024-02-04 21:56:00 +0800

    new file

---
+++ components/addNewAccounting.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- 編輯一筆 -->
+    <div class="addAccounting">
+        <label for="" class="addAccounting__title">日期</label>
+        <input type="date" class="addAccounting__date">
+
+        <label for="" class="addAccounting__title">名稱</label>
+        <input type="text" class="addAccounting__text">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.addAccounting {
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 10px;
+
+    &__title {
+        font-weight: 600;
+        text-align: center;
+    }
+
+    &__date {
+        width: 163px;
+        border-radius: 4px;
+        border: 1px solid black;
+    }
+
+    &__text {
+        width: 160px;
+        border-radius: 4px;
+        border: 1px solid black;
+
+    }
+
+    &__select {
+        width: 166px;
+        background-color: #F5D0D0;
+        border-radius: 4px;
+        border: 1px solid black;
+
+    }
+}
+</style>
---
+++ components/editNewAccounting.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 編輯一筆 -->
+    <div class="editAccounting">
+        <label for="" class="editAccounting__title">日期</label>
+        <input type="date" class="editAccounting__date">
+
+        <label for="" class="editAccounting__title">名稱</label>
+        <input type="text" class="editAccounting__text">
+
+        <label for="" class="editAccounting__title">狀態</label>
+        <select name="" id="" class="editAccounting__select">
+            <option value="1">尚未結清</option>
+            <option value="2">已經結清</option>
+        </select>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.editAccounting {
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 10px;
+
+    &__title {
+        font-weight: 600;
+        text-align: center;
+    }
+
+    &__date {
+        width: 163px;
+        border-radius: 4px;
+        border: 1px solid black;
+    }
+
+    &__text {
+        width: 160px;
+        border-radius: 4px;
+        border: 1px solid black;
+
+    }
+
+    &__select {
+        width: 166px;
+        background-color: #F5D0D0;
+        border-radius: 4px;
+        border: 1px solid black;
+
+    }
+}
+</style>
